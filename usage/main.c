4f664c57552daa2935ef19f5c34aba4fdcb2b3f7
Riccardo Sieve <sievericcardo@gmail.com>
2018-11-18 19:05:44 +0100

    Removed commented code (not needed)

--- usage/main.c
+++ usage/main.c
@@ -9,21 +9,11 @@ int main() {
         exit(-1);
     }
 
-    //char *string_array[] = {"Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "Suspendisse", "scelerisque", "leo", "quis", "urna", "tempor", "id" "hendrerit", "felis", "commodo", "Ut", "ex", "magna", "aliquam", "non", "lorem", "in", "vestibulum", "pharetra", "justo"};
-
     ArrayList* arraylist_string = ArrayList_create();
-
-    // for(int i=0; i<28; i++) {
-    //     printf("%s\n", string_array[i]);
-    //     ArrayList_add(arraylist_string, (char*)new_string(string_array[i]));
-    //     printf("Intermediate value %s\n", (char*)arraylist_string->data[i]);
-    // }
 
     //int i=0;
     while(fscanf(fin, "%s", buffer) != EOF) {
         ArrayList_add(arraylist_string, (char*)new_string(buffer));
-        // printf("%s ", (char*)arraylist_string->data[i]);
-        // i++;
     }
 
     printf("Printing maybe\n");
